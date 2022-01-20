cdb20767f470fffe10b3200cf69047a7f5026b21
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-01-20 09:36:48 -0500

    got rid of commas

--- Tester.java
+++ Tester.java
@@ -3,24 +3,51 @@ import java.util.ArrayList;
 public class Tester{
 	public static void main(String args[]){
 		ArrayList<ArrayList<Character>> board = new ArrayList<ArrayList<Character>>();
-
-		for(int i = 0; i<10;i++){
+		for(int i = 0; i<20;i++){
 			board.add(new ArrayList<Character>());
-			for(int k = 0; k<9;k++){
-				if(k<4){
-					board.get(i).add('?');
-				}else if(k>4){
-					board.get(i).add('O');
+			for(int k = 0; k<7;k++){
+
+				if(i%2 != 0){
+					
+					if(k<4){
+						board.get(i).add(' ');
+					}else if(k>4){
+						board.get(i).add('O');
+					}else{
+						board.get(i).add('	');
+					}
 				}else{
-					board.get(i).add('	');
+					if(k<4){
+						board.get(i).add('?');
+					}else if(k>4){
+						board.get(i).add('O');
+					}else{
+						board.get(i).add('	');
+					}
 				}
 			}
 		}
 
 		System.out.print("   BOARD:");
 		System.out.println("           SCORE:");
+		
+		int counter = 0;
+		System.out.println("---------------------");
 		for(int i = 0; i<board.size();i++){
-			System.out.println(board.get(i));
-		}
+			if(counter ==2){
+				System.out.println("---------------------");
+				counter = -1;
+				i--;
+			}else{
+				String printBoard = board.get(i).toString()
+				.replace(",", "")  
+				.replace("[", "|")  
+				.replace("]", "|")  
+				.trim(); 
+				System.out.println(printBoard);
+			}
+			counter++;
+		}	
+		System.out.println("---------------------");
 	}
 }
