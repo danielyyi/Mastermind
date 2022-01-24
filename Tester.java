cc76f51ac08734f6774574cf1c6d6ac9a62caab4
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-01-24 09:30:31 -0500

    made Tester

--- Tester.java
+++ Tester.java
@@ -1,53 +1,25 @@
+import java.util.Scanner;
 import java.util.ArrayList;
-
 public class Tester{
 	public static void main(String args[]){
-		ArrayList<ArrayList<Character>> board = new ArrayList<ArrayList<Character>>();
-		for(int i = 0; i<20;i++){
-			board.add(new ArrayList<Character>());
-			for(int k = 0; k<7;k++){
-
-				if(i%2 != 0){
-					
-					if(k<4){
-						board.get(i).add(' ');
-					}else if(k>4){
-						board.get(i).add('O');
-					}else{
-						board.get(i).add('	');
-					}
-				}else{
-					if(k<4){
-						board.get(i).add('?');
-					}else if(k>4){
-						board.get(i).add('O');
-					}else{
-						board.get(i).add('	');
-					}
-				}
-			}
-		}
-
-		System.out.print("   BOARD:");
-		System.out.println("           SCORE:");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Hey Kids! Welcome to Mastermind!");
+		System.out.print("Press [Enter] to Play: ");
+		scan.nextLine();
 		
-		int counter = 0;
-		System.out.println("---------------------");
-		for(int i = 0; i<board.size();i++){
-			if(counter ==2){
-				System.out.println("---------------------");
-				counter = -1;
-				i--;
-			}else{
-				String printBoard = board.get(i).toString()
-				.replace(",", "")  
-				.replace("[", "|")  
-				.replace("]", "|")  
-				.trim(); 
-				System.out.println(printBoard);
-			}
-			counter++;
-		}	
-		System.out.println("---------------------");
+		Game g = new Game();
+		boolean playing = true;
+		
+		while(playing){
+			Scanner scan2 = new Scanner(System.in);
+			System.out.print("Type 4 letters as your guess (R, O, Y, G, B, P, W):");
+			String input = scan2.nextLine();
+			g.convert(input);
+			g.printBoard();
+		}
+		
+		
+		
+		g.printBoard();
 	}
 }
