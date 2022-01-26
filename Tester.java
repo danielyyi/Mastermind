cad7273ba1f2c74f46764ab87ac89fa2b1b78698
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-01-26 09:36:08 -0500

    added spacing

--- Tester.java
+++ Tester.java
@@ -3,23 +3,22 @@ import java.util.ArrayList;
 public class Tester{
 	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		System.out.println("Hey Kids! Welcome to Mastermind!");
 		System.out.print("Press [Enter] to Play: ");
 		scan.nextLine();
 		
 		Game g = new Game();
 		boolean playing = true;
-		
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		while(playing){
 			Scanner scan2 = new Scanner(System.in);
-			System.out.print("Type 4 letters as your guess (R, O, Y, G, B, P, W):");
+			
+			System.out.println("Possible Pin Colors: R, O, Y, G, B, P, W");
+			System.out.print("Input 4 Pin Colors To Guess: ");
 			String input = scan2.nextLine();
 			g.convert(input);
-			g.printBoard();
 		}
-		
-		
-		
 		g.printBoard();
 	}
 }
