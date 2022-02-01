60ef49d71307b7ecf0776450d3d4c7259616c3cf
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-02-01 09:34:55 -0500

    added comments and replays

--- Game.java
+++ Game.java
@@ -1,13 +1,21 @@
+/*This is the the class that handles the mechanics of the game:
+ * Game constructor
+ * guess function
+ * printBoard function*/
 import java.util.ArrayList;
 public class Game{
 	ArrayList<ArrayList<Character>> board;
 	ArrayList<Character> code;
-	ArrayList<Character> bank;
+	ArrayList<Character> bank; //this will be filled with all the possible letters and the code will randomly grab from it
 	private int pos;
+	/*This constructor:
+	 * assigns the ArrayLists above
+	 * makes the main board by adding chars
+	 * generates the secret code*/
 	public Game(){
 		pos = 0;
 		board = new ArrayList<ArrayList<Character>>();
-		//make board------------------------------------------
+		//make board
 		for(int i = 0; i<20;i++){
 			board.add(new ArrayList<Character>());
 			for(int k = 0; k<7;k++){
@@ -32,27 +40,32 @@ public class Game{
 				}
 			}
 		}
-		//----------------------------------------------------
 		
-		//make code board-------------------------------------
+		//generate code ArrayList
 		bank = new ArrayList<Character>();
 		bank.add('R');bank.add('Y');bank.add('O');bank.add('G');bank.add('B');bank.add('P');bank.add('W');
 		code = new ArrayList<Character>();
-		for(int i = 0; i<4; i++){
+		for(int i = 0; i<4; i++){//grab from the bank and remove it
 			int rand = (int)(Math.random()* bank.size());
 			code.add(bank.get(rand));
 			bank.remove(rand);
 		}
-		//----------------------------------------------------
 	}
-	public boolean guess(String input){
+	
+	
+	/*this guess function:
+	 * takes in a string (the player's guess) and returns an int: 0=keep playing, 1=lose, 2=win
+	 * puts the player guesses on the board
+	 * checks to see if the guesses match with the code and randomly places the B and W indicator pins in the designated spots
+	 * checks to see what number to return*/
+	public int guess(String input){
 		ArrayList<Character> guess = new ArrayList<Character>();
 		ArrayList<Character> pins = new ArrayList<Character>();
 		
 		for(int i = 0; i<4;i++){
-			guess.add(input.charAt(i));
+			guess.add(input.charAt(i)); //add player guess to board
 			for(int k = 0; k<4; k++){
-				if(guess.get(i) == code.get(k)){
+				if(guess.get(i) == code.get(k)){//add B and W pins accordingly
 					if(k==i){
 						pins.add('B');
 					}else{
@@ -63,6 +76,7 @@ public class Game{
 			board.get(pos).set(i, guess.get(i));
 		}
 		
+		//these are for scrambling the order of the B and W pins
 		ArrayList<Integer> unscrambled = new ArrayList<Integer>();
 		unscrambled.add(0);unscrambled.add(1);unscrambled.add(2);unscrambled.add(3);
 		ArrayList<Integer> scrambled = new ArrayList<Integer>();
@@ -73,6 +87,7 @@ public class Game{
 			unscrambled.remove(rand);
 		}
 		
+		//places the scrambled pins in each possible position
 		for(int i = 0; i<pins.size();i++){
 			if(i == scrambled.get(0)){
 				board.get(pos).set(5, pins.get(i));
@@ -86,28 +101,41 @@ public class Game{
 			}
 		}
 		
-		pos+=2;
+		pos+=2; //this shifts down the board so new guess are in lower rows, etc.
 		unscrambled.add(0);unscrambled.add(1);unscrambled.add(2);unscrambled.add(3);
-		System.out.println(pins);
 		printBoard();
 		
+		//check if game is over, win, lose, etc.
+		int counter = 0;
+		for(int i = 0; i<pins.size();i++){
+			if(pins.get(i) == 'B'){
+				counter++;
+			}
+		}
+		if(counter == 4){
+			return 2;//wins
+		}
+		counter = 0;
 		if(pos == 20){
-			return false;
+			return 1;//loses
 		}
 		
-		return true;
+		return 0;//keep playing
 	}
+	
+	//this printBoard function prints to board with dividers and also converts it to a string so I can take out all the brackets and commas
 	public void printBoard(){
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		System.out.println(" BOARD:        SCORE:");
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		System.out.println(" BOARD:        PINS:");
 		int counter = 0;
 		System.out.println("---------------------");
-		for(int i = 0; i<board.size();i++){
+		for(int i = 0; i<board.size();i++){//places dividers nicely
 			if(counter ==2){
-				System.out.println("---------------------");
+				System.out.println("---------------------")
 				counter = -1;
 				i--;
 			}else{
+				//get rid of brackets and commas
 				String printBoard = board.get(i).toString()
 				.replace(",", "")  
 				.replace("[", "|")  
