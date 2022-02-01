a6448cf3289bed76ff29544d7b02af2625cb2bc8
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-02-01 09:35:18 -0500

    added comments, instructions, and replays

--- Tester.java
+++ Tester.java
@@ -1,24 +1,68 @@
+/*this is the Tester class that handles the UI and player inputs:
+ * main function
+ * playGame function*/
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Tester{
+	/*this main function:
+	 * explains how to play
+	 * calls playGame function to begin*/
 	public static void main(String args[]){
 		Scanner scan = new Scanner(System.in);
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		System.out.println("Hey Kids! Welcome to Mastermind!");
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		System.out.println("HOW TO PLAY:");
+		System.out.println("- Your job is to guess the 4 letter code set by your opponent.");
+		System.out.println("- If you see a W pin on the right of the board, you know you have a correct letter but in the wrong order.");
+		System.out.println("- If you see a B pin, you know you have a correct letter in the right order.");
+		System.out.println("- The pin positions don't correspond to the order of the words.");
+		System.out.println("- You have 10 tries to guess the code correctly. Letters do not repeat.");
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
 		System.out.print("Press [Enter] to Play: ");
 		scan.nextLine();
+		playGame();
 		
+	}
+	
+	/*this playGame function:
+	 * takes in a player guess and checks if it valid
+	 * then it calls the Game class and passes in the player's guess
+	 * if the player wins, loses, etc. then it prints accordingly and asks to play again*/
+	public static void playGame(){
+		Scanner scan = new Scanner(System.in);
 		Game g = new Game();
-		boolean playing = true;
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		while(playing){
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		int status = 0;
+		while(status == 0){//keeps going if keep playing: 1=lose, 2 = win
 			Scanner scan2 = new Scanner(System.in);
 			
-			System.out.println("Possible Pin Colors: R, O, Y, G, B, P, W");
-			System.out.print("Input 4 Pin Colors To Guess: ");
+			System.out.println("Possible Letters: R, O, Y, G, B, P, W");
+			System.out.print("Input 4 Letters To Guess: ");
 			String input = scan2.nextLine();
-			playing = g.guess(input);
+			//if not 4 letters
+			if(input.length() != 4){
+				System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+				System.out.println("INPUT ERROR: Your code must be 4 letters long!!!");
+			//if not a letter
+			}else if(Character.isLetter(input.charAt(0)) == false || Character.isLetter(input.charAt(1)) == false || Character.isLetter(input.charAt(2)) == false || Character.isLetter(input.charAt(3)) == false){
+				System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+				System.out.println("INPUT ERROR: Letters only please!!!");
+			}else{
+				//even if the input is lower case, still work
+				status = g.guess(input.toUpperCase());
+			}
+			
 		}
-		g.printBoard();
+		
+		if(status == 1){
+			System.out.println("OH NO! YOU RAN OUT OF GUESSES!! YOU LOSE!!");
+		}else{
+			System.out.println("YAY! YOU WIN!");
+		}
+		
+		System.out.print("\nPRESS [ENTER] TO PLAY AGAIN: ");
+		scan.nextLine();
+		playGame();
 	}
 }
